54ca9599ec0265447e988ce76c515955fe28b873
Artem Savelyev <04012008artem@gmail.com>
2025-12-18 08:35:00 +0500

    14 лаба

--- Savelyev41razmer/OrderWindow.xaml.cs
+++ Savelyev41razmer/OrderWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Savelyev41razmer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,40 +18,356 @@ namespace Savelyev41razmer
     /// <summary>
     /// Логика взаимодействия для OrderWindow.xaml
     /// </summary>
-    //public partial class OrderWindow : Window
-    //{
-    //    List<OrderProduct> selectedOrderProducts = new List<OrderProduct>();
-    //    List<Product> selectedProducts = new List<Product>();
-    //    private Order currentOrder = new Order();
-    //    private OrderProduct currentOrderProduct = new OrderProduct();
-    //    public OrderWindow()
-    //    {
-    //        InitializeComponent();
-    //    }
+    public partial class OrderWindow : Window
+    {
+        private List<OrderProduct> selectedOrderProducts = new List<OrderProduct>();
+        private List<Product> selectedProducts = new List<Product>();
+        private Order currentOrder = new Order();
+        private User _currentUser;
+        private Product product;
 
-    //    public OrderWindow(List<OrderProduct> selectedOrderProducts, List<Product> selectedProducts, string FIO)
-    //    {
-    //        InitializeComponent();
-    //        var currentPickups = Savelyev41Entities1.GetContext().PickUpPoint.ToList();
-    //        PickupCombo.ItemsSource = currentPickups;
+        public OrderWindow(List<OrderProduct> selectedOrderProducts, List<Product> selectedProducts, User user)
+        {
+            InitializeComponent();
+            var currentPickups = Savelyev41Entities1.GetContext().PickUpPoint.ToList();
+            PickUpPointCombo.ItemsSource = currentPickups;
+            if (user != null)
+            {
+                ClientTB.Text = $"{user.UserSurname} {user.UserName} {user.UserPatronymic}";
+            }
+            else
+            {
+                ClientTB.Text = "Гость";
+            }
 
-    //        ClientTB.Text = FIO;
-    //        TBOrderID.Text = selectedOrderProducts.First().OrderID.ToString();
+            _currentUser = user;
 
-    //        ShoeListView.ItemsSource = selectedProducts;
 
-    //        foreach (Product p in selectedProducts)
-    //        {
-    //            p.ProductQuantityInStock = 1;
-    //            foreach (OrderProduct q in selectedOrderProducts)
-    //            {
-    //                if (p.ProductArticleNumber == q.ProductArticleNumber)
-    //                {
-    //                    p.ProductQuantityInStock = q.ProductQuantityInStock;
-    //                }
-    //            }
+            this.selectedOrderProducts = selectedOrderProducts;
+            this.selectedProducts = selectedProducts;
 
-    //        }
-    //    }
-    //}
+            foreach (Product p in selectedProducts)
+            {
+                p.Quantity = 1;
+                foreach (OrderProduct q in selectedOrderProducts)
+                {
+                    if (p.ProductArticleNumber == q.ProductArticleNumber)
+                        p.Quantity = q.Count;
+                }
+            }
+
+            ShoeListView.ItemsSource = selectedProducts;
+            DateTime orderDate = DateTime.Now;
+
+            var lastOrder = Savelyev41Entities1.GetContext().Order.OrderByDescending(o => o.OrderID).FirstOrDefault();
+            int newOrderNumber = lastOrder != null ? lastOrder.OrderID + 1 : 1;
+            OrderID.Text = newOrderNumber.ToString();
+            SetDeliveryDate();
+            UpdateTotals();
+        }
+
+        private void SetDeliveryDate()
+        {
+            bool fastDeliveryPossible = true;
+
+            foreach (Product product in selectedProducts)
+            {
+                var orderProduct = selectedOrderProducts
+                    .FirstOrDefault(op => op.ProductArticleNumber == product.ProductArticleNumber);
+
+                if (orderProduct != null)
+                {
+                    if (orderProduct.Count > product.ProductQuantityInStock || product.ProductQuantityInStock < 3)
+                    {
+                        fastDeliveryPossible = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    fastDeliveryPossible = false;
+                    break;
+                }
+            }
+            int deliveryDays = fastDeliveryPossible ? 3 : 6;
+
+            DateTime orderDate = DateTime.Now;
+            OrderDP.Text = DateTime.Now.ToString("dd.MM.yyyy");
+
+            var deliveryDate = orderDate.AddDays(deliveryDays);
+
+
+            if (OrderDeliveryDate != null)
+            {
+                OrderDeliveryDate.Text = deliveryDate.ToString("dd.MM.yyyy");
+            }
+            else
+            {
+                MessageBox.Show($"Дата доставки: {deliveryDate.ToString("dd.MM.yyyy")}");
+            }
+        }
+
+        private void UpdateTotals()
+        {
+            decimal totalAmount = 0;
+            decimal totalDiscount = 0;
+
+            foreach (Product product in selectedProducts)
+            {
+                foreach (OrderProduct op in selectedOrderProducts)
+                {
+                    if (product.ProductArticleNumber == op.ProductArticleNumber)
+                    {
+                        int quantity = op.Count;
+                        totalAmount += product.ProductCost * quantity;
+                        totalDiscount += product.ProductCost * quantity * (product.ProductDiscountAmount / 100m);
+                    }
+                }
+            }
+
+            decimal finalAmount = totalAmount - totalDiscount;
+
+        }
+
+        private void OrderDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetDeliveryDate();
+        }
+
+        private void minusButton_Click(object sender, RoutedEventArgs e)
+        {
+            var prod = (sender as Button).DataContext as Product;
+            if (prod == null) return;
+
+            if (prod.Quantity > 1)
+            {
+                prod.Quantity--;
+                var selectedOP = selectedOrderProducts.FirstOrDefault(p => p.ProductArticleNumber == prod.ProductArticleNumber);
+                if (selectedOP != null)
+                {
+                    int index = selectedOrderProducts.IndexOf(selectedOP);
+                    selectedOrderProducts[index].Count--;
+                }
+                SetDeliveryDate();
+                UpdateTotals();
+                ShoeListView.Items.Refresh();
+            }
+        }
+
+        private void plusButton_Click(object sender, RoutedEventArgs e)
+        {
+            var prod = (sender as Button).DataContext as Product;
+            if (prod == null) return;
+
+            prod.Quantity++;
+
+            var selectedOP = selectedOrderProducts.FirstOrDefault(p => p.ProductArticleNumber == prod.ProductArticleNumber);
+            if (selectedOP != null)
+            {
+                int index = selectedOrderProducts.IndexOf(selectedOP);
+                selectedOrderProducts[index].Count++;
+            }
+
+            SetDeliveryDate();
+            UpdateTotals();
+            ShoeListView.Items.Refresh();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.DataContext is Product product)
+            {
+                var result = MessageBox.Show("Удалить товар из заказа?",
+                    "Подтверждение", MessageBoxButton.YesNo);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    selectedProducts.Remove(product);
+                    var orderProduct = selectedOrderProducts.FirstOrDefault(op => op.ProductArticleNumber == product.ProductArticleNumber);
+                    if (orderProduct != null)
+                    {
+                        selectedOrderProducts.Remove(orderProduct);
+                    }
+
+                    ShoeListView.ItemsSource = null;
+                    ShoeListView.ItemsSource = selectedProducts;
+                    SetDeliveryDate();
+                    UpdateTotals();
+
+                    if (selectedProducts.Count == 0)
+                    {
+                        MessageBox.Show("Заказ пуст");
+                        ProductPage.ClearOrderItems();
+                        this.DialogResult = false;
+                        this.Close();
+                    }
+                }
+            }
+        }
+
+        private void SaveOrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (selectedProducts.Count == 0)
+                {
+                    MessageBox.Show("Добавьте товары в заказ!");
+                    return;
+                }
+
+                if (PickUpPointCombo.SelectedItem == null)
+                {
+                    MessageBox.Show("Выберите пункт выдачи!");
+                    return;
+                }
+
+                var selectedPickupPoint = PickUpPointCombo.SelectedItem as PickUpPoint;
+                var context = Savelyev41Entities1.GetContext();
+
+                DateTime orderDate = DateTime.Now;
+                DateTime deliveryDate;
+                if (!DateTime.TryParse(OrderDP.Text, out deliveryDate))
+                {
+                    deliveryDate = orderDate.AddDays(3);
+                }
+
+                int? clientId = null;
+                if (_currentUser != null)
+                {
+                    var existingUser = context.User.FirstOrDefault(u => u.UserID == _currentUser.UserID);
+                    if (existingUser != null)
+                    {
+                        clientId = _currentUser.UserID;
+                    }
+                    else
+                    {
+                        clientId = null;
+                    }
+                }
+
+                currentOrder = new Order
+                {
+                    OrderDate = orderDate,
+                    OrderDeliveryDate = deliveryDate,
+                    OrderPickupPoint = selectedPickupPoint.PickUpPointID,
+                    OrderClientID = clientId,
+                    OrderCode = new Random().Next(100000, 999999),
+                    OrderStatus = "Новый"
+                };
+
+                context.Order.Add(currentOrder);
+                context.SaveChanges();
+
+                foreach (var op in selectedOrderProducts)
+                {
+                    op.OrderID = currentOrder.OrderID;
+                    context.OrderProduct.Add(op);
+
+                    var product = context.Product.FirstOrDefault(p => p.ProductArticleNumber == op.ProductArticleNumber);
+                    if (product != null)
+                    {
+                        product.ProductQuantityInStock -= op.Count;
+                    }
+                }
+
+                context.SaveChanges();
+
+                MessageBox.Show($"Заказ №{currentOrder.OrderID} оформлен!\nКод: {currentOrder.OrderCode}",
+                    "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                this.DialogResult = true;
+                this.Close();
+                ProductPage.ClearOrderItems();
+            }
+            catch (System.Data.Entity.Infrastructure.DbUpdateException dbEx)
+            {
+                if (dbEx.InnerException?.InnerException is System.Data.SqlClient.SqlException sqlEx)
+                {
+                    if (sqlEx.Number == 547)
+                    {
+                        MessageBox.Show($"Ошибка: Невозможно сохранить заказ. " +
+                            $"Проверьте корректность данных пользователя (UserID: {_currentUser?.UserID}).",
+                            "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                        SaveOrderAsGuest();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Ошибка при сохранении заказа: {dbEx.Message}",
+                        "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SaveOrderAsGuest()
+        {
+            try
+            {
+                var selectedPickupPoint = PickUpPointCombo.SelectedItem as PickUpPoint;
+                var context = Savelyev41Entities1.GetContext();
+
+                DateTime deliveryDate;
+                if (!DateTime.TryParse(OrderDP.Text, out deliveryDate))
+                {
+                    deliveryDate = DateTime.Now.AddDays(3);
+                }
+
+                currentOrder = new Order
+                {
+                    OrderDate = DateTime.Now,
+                    OrderDeliveryDate = deliveryDate,
+                    OrderPickupPoint = selectedPickupPoint.PickUpPointID,
+                    OrderClientID = null,
+                    OrderCode = new Random().Next(100000, 999999),
+                    OrderStatus = "Новый"
+                };
+
+                context.Order.Add(currentOrder);
+                context.SaveChanges();
+
+                foreach (var op in selectedOrderProducts)
+                {
+                    op.OrderID = currentOrder.OrderID;
+                    context.OrderProduct.Add(op);
+
+                    var product = context.Product.FirstOrDefault(p => p.ProductArticleNumber == op.ProductArticleNumber);
+                    if (product != null)
+                    {
+                        product.ProductQuantityInStock -= op.Count;
+                    }
+                }
+
+                context.SaveChanges();
+
+                MessageBox.Show($"Гостевой заказ №{currentOrder.OrderID} оформлен!\nКод: {currentOrder.OrderCode}",
+                    "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                this.DialogResult = true;
+                this.Close();
+                ProductPage.ClearOrderItems();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении гостевого заказа: {ex.Message}",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+    public partial class Product
+    {
+        public int Quantity { get; set; }
+        public decimal TotalPrice => ProductCost * Quantity;
+    }
+
+    public partial class PickUpPoint
+    {
+        public string FullAddress => $"{PickUpPointIndex} г. {PickUpCity}, ул. {PickUpStreet}, д. {PickUpHouse}";
+    }
 }
--- Savelyev41razmer/ProductPage.xaml.cs
+++ Savelyev41razmer/ProductPage.xaml.cs
@@ -20,79 +20,96 @@ namespace Savelyev41razmer
     /// </summary>
     public partial class ProductPage : Page
     {
-        int CurrentNumRecords, AllNumRecords;
+        public static List<Product> CurrentProducts = new List<Product>();
+        private User _currentUser;
+        private static Dictionary<string, int> _orderItems = new Dictionary<string, int>();
 
+        public static event Action OnCartCleared;
+
+        public static void ClearOrderItems()
+        {
+            _orderItems.Clear();
+            OnCartCleared?.Invoke();
+        }
         public ProductPage(User user)
         {
             InitializeComponent();
-
-            FIOTB.Text = user.UserSurname + " " + user.UserName + " " + user.UserPatronymic;
-            switch (user.UserRole)
+            _currentUser = user;
+            OnCartCleared += () =>
             {
-                case 1:
-                    RoleTB.Text = "Клиент"; break;
-                case 2:
-                    RoleTB.Text = "Менеджер"; break;
-                case 3:
-                    RoleTB.Text = "Администратор"; break;
-                case 4:
-                    RoleTB.Text = "Гость"; break;
-
+                Dispatcher.Invoke(() =>
+                {
+                    OrderBtn.Visibility = Visibility.Collapsed;
+                });
+            };
+            Loaded += (s, e) =>
+            {
+                if (_orderItems.Count == 0)
+                {
+                    OrderBtn.Visibility = Visibility.Collapsed;
+                }
+            };
+            if (user != null)
+            {
+                FIOTB.Text = user.UserSurname + " " + user.UserName + " " + user.UserPatronymic;
+                switch (user.UserRole)
+                {
+                    case 1:
+                        RoleTB.Text = "Клиент"; break;
+                    case 2:
+                        RoleTB.Text = "Менеджер"; break;
+                    case 3:
+                        RoleTB.Text = "Администратор"; break;
+                    case 4:
+                        RoleTB.Text = "Гость"; break;
+                }
             }
-            
-
-            var currentProducts = Savelyev41Entities1.GetContext().Product.ToList();
-
-            ProductListView.ItemsSource = currentProducts;
+            else
+            {
+                FIOTB.Text = " Вы вошли как гость";
+            }
+            var currentProduct = Savelyev41Entities1.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currentProduct;
 
             ComboType.SelectedIndex = 0;
+            UpdateProduct();
+            UpdateOrderButtonVisibility();
         }
-        private void UpdateProducts()
+        public static event EventHandler OrderItemsChanged;
+
+        private static void OnOrderItemsChanged()
         {
-            var currentProducts = Savelyev41Entities1.GetContext().Product.ToList();
-
-            AllNumRecords = currentProducts.Count;
-
-            if (ComboType.SelectedIndex == 0)
-            {
-                currentProducts = currentProducts.Where(p => (p.ProductDiscountAmount) >= 0 && (p.ProductDiscountAmount) <= 100).ToList();
-            }
-
-            if (ComboType.SelectedIndex == 1)
-            {
-                currentProducts = currentProducts.Where(p => (p.ProductDiscountAmount) >= 0 && (p.ProductDiscountAmount) < 10).ToList();
-            }
-
-            if (ComboType.SelectedIndex == 2)
-            {
-                currentProducts = currentProducts.Where(p => (p.ProductDiscountAmount) >= 10 && (p.ProductDiscountAmount) < 15).ToList();
-            }
-
-            if (ComboType.SelectedIndex == 3)
-            {
-                currentProducts = currentProducts.Where(p => (p.ProductDiscountAmount) >= 15 && (p.ProductDiscountAmount) <= 100).ToList();
-            }
-
-            currentProducts = currentProducts.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
-
-            //ProductListView.ItemsSource = currentProducts.ToList();
-
-            if (RButtonUp.IsChecked.Value)
-            {
-                currentProducts = currentProducts.OrderBy(p => p.ProductCost).ToList();
-            }
-
-            if (RButtonDown.IsChecked.Value)
-            {
-                currentProducts = currentProducts.OrderByDescending(p => p.ProductCost).ToList();
-            }
-
-            CurrentNumRecords = currentProducts.Count;
-           
-            ProductListView.ItemsSource = currentProducts;
-
-            TBAllRecords.Text = CurrentNumRecords.ToString() + " из " + AllNumRecords.ToString();
+            OrderItemsChanged?.Invoke(null, EventArgs.Empty);
         }
+        private void AddToOrder(Product product)
+        {
+            string article = product.ProductArticleNumber;
+
+            if (_orderItems.ContainsKey(article))
+            {
+                _orderItems[article]++;
+            }
+            else
+            {
+                _orderItems[article] = 1;
+            }
+
+            UpdateOrderButtonVisibility();
+            MessageBox.Show($"Товар \"{product.ProductName}\" добавлен к заказу");
+            OnOrderItemsChanged();
+            UpdateOrderButtonVisibility();
+        }
+        private void UpdateOrderButtonVisibility()
+        {
+            bool hasItems = _orderItems.Count > 0;
+
+            int totalCount = _orderItems.Sum(item => item.Value);
+            hasItems = totalCount > 0;
+
+            OrderBtn.Visibility = hasItems ? Visibility.Visible : Visibility.Collapsed;
+
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
@@ -100,25 +117,121 @@ namespace Savelyev41razmer
 
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            UpdateProducts();
+            UpdateProduct();
         }
+
         private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            UpdateProducts();
+            UpdateProduct();
         }
+
+        //private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
         private void RButtonUp_Checked(object sender, RoutedEventArgs e)
         {
-            UpdateProducts();
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-
+            UpdateProduct();
         }
 
         private void RButtonDown_Checked(object sender, RoutedEventArgs e)
         {
-            UpdateProducts();
-        } 
+            UpdateProduct();
+        }
+
+        int count;
+        int count2;
+        private void UpdateProduct()
+        {
+            var currentProduct = Savelyev41Entities1.GetContext().Product.ToList();
+            count = currentProduct.Count;
+
+            if (TBoxSearch.Text.Length > 0)
+                currentProduct = currentProduct.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount > 0 && p.ProductDiscountAmount <= 9)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount > 9 && p.ProductDiscountAmount <= 14)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount > 14 && p.ProductDiscountAmount <= 100)).ToList();
+            }
+
+            currentProduct = currentProduct.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            ProductListView.ItemsSource = currentProduct.ToList();
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                ProductListView.ItemsSource = currentProduct.OrderByDescending(p => p.ProductCost).ToList();
+            }
+            if (RButtonUp.IsChecked.Value)
+            {
+                ProductListView.ItemsSource = currentProduct.OrderBy(p => p.ProductCost).ToList();
+            }
+
+            count2 = currentProduct.Count;
+            TBAllRecords.Text = "кол-во " + count2.ToString() + " из " + count.ToString();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProductListView.SelectedItem is Product selectedProduct)
+            {
+                AddToOrder(selectedProduct);
+            }
+        }
+
+        private void OrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedOrderProducts = new List<OrderProduct>();
+            var selectedProducts = new List<Product>();
+
+            foreach (var item in _orderItems)
+            {
+                var product = Savelyev41Entities1.GetContext().Product
+                    .FirstOrDefault(p => p.ProductArticleNumber == item.Key);
+
+                if (product != null)
+                {
+                    selectedProducts.Add(product);
+                    var orderProduct = new OrderProduct
+                    {
+                        ProductArticleNumber = product.ProductArticleNumber,
+                        Count = item.Value
+                    };
+                    selectedOrderProducts.Add(orderProduct);
+                }
+            }
+
+            var orderWindow = new OrderWindow(selectedOrderProducts, selectedProducts, _currentUser);
+            orderWindow.Owner = Application.Current.MainWindow;
+
+            orderWindow.Closed += (s, args) =>
+            {
+                UpdateOrderButtonVisibility();
+
+                if (_orderItems.Count == 0)
+                {
+                    OrderBtn.Visibility = Visibility.Collapsed;
+                }
+            };
+
+            orderWindow.ShowDialog();
+
+            UpdateOrderButtonVisibility();
+        }
+
     }
 }
